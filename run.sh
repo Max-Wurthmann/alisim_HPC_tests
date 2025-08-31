0c3854beb3addbad28bcd930881754b54662f860
Max-Wurthmann <mx.wurthmann@gmail.com>
2025-08-31 19:17:54 +0200

    extracted run into function

--- run.sh
+++ run.sh
@@ -15,66 +15,73 @@ out_logfile=outlog.tmp
 data_file=data.csv
 output_dir=output
 
-# clear tmp logfile if it exists
-rm -f $mem_logfile
-# create output directory if it doesn't exist
-mkdir -p $output_dir
-# create data_file if not exists
-if [[ ! -f $data_file ]]; then
-  # schema of csv
-  echo 'peakRSS, runtime, full command, datetime of run' >$data_file
-fi
+run_experimment() {
+  echo "Running experiment with $n_sites sites, $n_taxa taxa, $n_alignments alignments, model $model"
+  echo "Using $n_proc processes, $n_threads threads per process, OpenMP algorithm $omp_alg"
 
-# save current datetime
-cur_datetime="$(date)"
+  # clear tmp logfile if it exists
+  rm -f $mem_logfile
+  # create output directory if it doesn't exist
+  mkdir -p $output_dir
+  # create data_file if not exists
+  if [[ ! -f $data_file ]]; then
+    # schema of csv
+    echo 'peakRSS, runtime, full command, datetime of run' >$data_file
+  fi
 
-# define command to run
-if [[ "$n_proc" -gt 1 ]]; then
-  # MPI version
-  cmd=(
-    mpirun -np "$n_proc" --allow-run-as-root
-    iqtree2-mpi --alisim "$output_dir/alg" -t "RANDOM{yh/$n_taxa}" -m "$model"
-    --length "$n_sites" --num-alignments "$n_alignments"
-    -nt "$n_threads"
-    --openmp-alg "$omp_alg"
-    -redo
-  )
-else
-  # single process version
-  cmd=(
-    iqtree2 --alisim "$output_dir/alg" -t "RANDOM{yh/$n_taxa}" -m "$model"
-    --length "$n_sites" --num-alignments "$n_alignments"
-    -nt "$n_threads"
-    --openmp-alg "$omp_alg"
-    -redo
-  )
-fi
+  # save current datetime
+  cur_datetime="$(date)"
 
-# start memory monitor in background
-while true; do
-  # Sum RSS of all processes matching iqtree2
-  ps -eo pid,rss,comm | awk 'BEGIN {sum = 0} /iqtree2/ {sum += $2} END {print sum}' >>$mem_logfile
-  sleep 1
-done &
+  # define command to run
+  if [[ "$n_proc" -gt 1 ]]; then
+    # MPI version
+    cmd=(
+      mpirun -np "$n_proc" --allow-run-as-root
+      iqtree2-mpi --alisim "$output_dir/alg" -t "RANDOM{yh/$n_taxa}" -m "$model"
+      --length "$n_sites" --num-alignments "$n_alignments"
+      -nt "$n_threads"
+      --openmp-alg "$omp_alg"
+      -redo
+    )
+  else
+    # single process version
+    cmd=(
+      iqtree2 --alisim "$output_dir/alg" -t "RANDOM{yh/$n_taxa}" -m "$model"
+      --length "$n_sites" --num-alignments "$n_alignments"
+      -nt "$n_threads"
+      --openmp-alg "$omp_alg"
+      -redo
+    )
+  fi
 
-# save pid of memory monitor
-pid=$!
+  # start memory monitor in background
+  while true; do
+    # Sum RSS of all processes matching iqtree2
+    ps -eo pid,rss,comm | awk 'BEGIN {sum = 0} /iqtree2/ {sum += $2} END {print sum}' >>$mem_logfile
+    sleep 1
+  done &
 
-# execute command, logging stdout to cmd.log while also printing to console
-"${cmd[@]}" | tee $out_logfile
+  # save pid of memory monitor
+  pid=$!
 
-# kill memory monitor
-kill -15 $pid
+  # execute command, logging stdout to cmd.log while also printing to console
+  "${cmd[@]}" >$out_logfile
 
-# calculate peak memory usage
-peakRSS=$(awk 'BEGIN{max=0} {if($1>max) max=$1} END{print max}' $mem_logfile)
+  # kill memory monitor
+  kill -15 $pid
 
-# grep runtime from out_logfile
-pattern='Simulation time: '
-runtime=$(grep "$pattern" $out_logfile)
-#cut pattern prefix
-runtime="${runtime#$pattern}"
+  # calculate peak memory usage
+  peakRSS=$(awk 'BEGIN{max=0} {if($1>max) max=$1} END{print max}' $mem_logfile)
 
-# record data
-# csv format: peakRSS, runtime, full command, current date/time
-echo "$peakRSS, $runtime, ${cmd[*]}, $cur_datetime" >>$data_file
+  # grep runtime from out_logfile
+  pattern='Simulation time: '
+  runtime=$(grep "$pattern" $out_logfile)
+  #cut pattern prefix
+  runtime="${runtime#$pattern}"
+
+  # record data
+  # csv format: peakRSS, runtime, full command, current date/time
+  echo "$peakRSS, $runtime, ${cmd[*]}, $cur_datetime" >>$data_file
+}
+
+run_experimment
