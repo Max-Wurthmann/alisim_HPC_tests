ca7d7d8e1188ad6f9a9e36d90f7e00c6fa396221
Max-Wurthmann <mx.wurthmann@gmail.com>
2025-09-03 16:08:58 +0200

    added hpc and mpi script, updated manual script, added manual, mpi data

--- run_manual.sh
+++ run_manual.sh
@@ -28,12 +28,11 @@ run_experimment() {
   mkdir -p $output_dir
 
   # run dummy command to sample random tree
-  cmd=(
-    iqtree2 --alisim "$output_dir/alg" -m "$model"
-    --length 1 --num-alignments 1
-    -r "$n_taxa"
-    -nt 1
-  )
+  iqtree2 --alisim "$output_dir/alg" -m 'JC' \
+    --length 1 --num-alignments 1 \
+    -r "$n_taxa" -nt 1 -redo \
+    >/dev/null 2>"$logs_dir/err.log"
+
   tree_file="$output_dir/alg.treefile"
 
   # save current datetime
@@ -52,20 +51,27 @@ run_experimment() {
   # save pids of all background processes
   pids=()
 
+  n_algnments_per_proc=$((n_alignments / n_procs))
+  remainder=$((n_alignments % n_procs))
+
   for ((i = 0; i < "$n_procs"; i++)); do
     out_logfile_i="$logs_dir/outlog$i.tmp"
     err_logfile_i="$logs_dir/err$i.log"
 
-    cmd=(
-      iqtree2 --alisim "$output_dir/alg$i" -m "$model"
-      --length "$n_sites" --num-alignments "$n_alignments"
-      -t "$tree_file"
-      -nt "$n_threads"
-      --openmp-alg "$omp_alg"
-    )
+    n_alignments_proc_i=$n_algnments_per_proc
+    if ((i < remainder)); then
+      n_alignments_proc_i=$((n_algnments_per_proc + 1))
+    fi
 
-    # execute command asychronously, logging stdout and stderr to file
-    "${cmd[@]}" >$out_logfile_i 2>>$err_logfile_i &
+    # execut command in background
+    iqtree2 --alisim "$output_dir/alg$i" -m "$model" \
+      --length "$n_sites" --num-alignments "$n_alignments_proc_i" \
+      -t "$tree_file" \
+      -nt "$n_threads" \
+      --openmp-alg "$omp_alg" \
+      -redo \
+      >$out_logfile_i 2>>$err_logfile_i &
+
     # save process id corresponding to the job that was just started
     pids[i]=$!
   done
@@ -77,7 +83,7 @@ run_experimment() {
     out_logfile_i="$logs_dir/outlog$i.tmp"
 
     # wait for job to finish
-    Wait "${pids[i]}"
+    wait "${pids[i]}"
 
     # grep runtime from out_logfile
     pattern='Simulation time: '
@@ -96,9 +102,11 @@ run_experimment() {
   # calculate peak memory usage
   peakRSS=$(awk 'BEGIN{max=0} {if($1>max) max=$1} END{print max}' $mem_logfile)
 
+  cmd='manual'
+
   # record data
   # csv format:  peakRSS,runtime,n_procs,n_threads,n_sites,n_taxa,n_alignments,omp_alg,full_command,datetime_of_run
-  echo "$peakRSS,$runtime_max,$n_procs,$n_threads,$n_sites,$n_taxa,$n_alignments,$omp_alg,${cmd[*]},$cur_datetime" >>$data_file
+  echo "$peakRSS,$runtime_max,$n_procs,$n_threads,$n_sites,$n_taxa,$n_alignments,$omp_alg,$cmd,$cur_datetime" >>$data_file
 }
 
 # define file names
